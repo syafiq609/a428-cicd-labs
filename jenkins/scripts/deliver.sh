34cd384d64ffb3a2ddf3520bdffed7daf7686ea4
syafiq609 <ekomalang40@gmail.com>
2025-08-10 13:28:30 +0700

    optimize deliver.sh with node_modules cache and auto serve

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,6 +6,12 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
+if [ ! -d "node_modules" ]; then
+  echo "📦 Installing dependencies (first time)..."
+  npm install
+else
+  echo "✅ Using cached dependencies..."
+fi
 export NODE_OPTIONS=--openssl-legacy-provider && npm run build
 set +x
 
