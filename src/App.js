23985e7d806985107ab1647b0fe198103bfbaed0
syafiq609 <ekomalang40@gmail.com>
2025-08-15 22:08:38 +0700

    kita mencoba webhooks2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Reactfarelimut123456789ayo semanagt semuanya jangan sampai sww </h1>
+          <h1 className="App-title">Welcome to Reactfarelimut</h1>
+        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
