68603f724ee214363f2bd367efc058c3a70a82aa
syafiq609 <ekomalang40@gmail.com>
2025-08-16 21:27:40 +0700

    terbaru

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Reactfarelimut123456789 allh hu akabar bismillahhirrahmanirahim gasz kuy</h1>
+          <h1 className="App-title">Welcome to Reactfarelimut123456789 allh hu akabar bismillahhirrahmanirahim gasz kuy salke dan zidan</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
